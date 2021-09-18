522d5fb1fcd3ac2a6bfefd47ef97ee51a6312bb5
eikkind@gmail.com <Rainb0w1nuS>
2021-09-18 09:06:02 +0300

    job4j_elementary

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Alexandr Korolkov");
+        System.out.println("07.04.1989");
+    }
+}
