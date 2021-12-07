7ac86d9872221375fad4abc535bb3238dc257d03
Eikind <eikkind@gmail.com>
2021-12-07 11:01:37 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Alexey Alexeev";
+        names[1] = "Boris Borisov";
+        names[2] = "Carl Carlov";
+        names[3] = "Denis Denisov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
